7999db7f922d30f60488800ce59e565280e38f92
Hou, Han <houhan@gmail.com>
2025-02-07 13:05:01 -0800

    WIP

--- src/aind_analysis_arch_result_access/util/s3.py
+++ src/aind_analysis_arch_result_access/util/s3.py
@@ -3,15 +3,24 @@ Util functions for public S3 bucket access
 '''
 import s3fs
 import pickle
+import json
 
 # The processed bucket is public
 fs = s3fs.S3FileSystem(anon=True)
 
 
-def get_df_from_s3_pkl(s3_path):
+def get_s3_pkl(s3_path):
     '''
     Load a pickled dataframe from an s3 path
     '''
     with fs.open(s3_path, 'rb') as f:
-        df = pickle.load(f)
-    return df
+        df_loaded = pickle.load(f)
+    return df_loaded
+
+def get_s3_json(s3_path):
+    '''
+    Load a json file from an s3 path
+    '''
+    with fs.open(s3_path) as f:
+        json_loaded = json.load(f)
+    return json_loaded
