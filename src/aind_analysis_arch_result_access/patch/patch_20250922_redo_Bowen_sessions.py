80ed5ed7aebc2a8703ad9e6a3c1e5dd3fb55bdc6
houhan@gmail.com <han.hou@alleninstitute.org>
2025-09-22 16:56:31 +0000

    copy all nwbs

--- src/aind_analysis_arch_result_access/patch/patch_20250922_redo_Bowen_sessions.py
+++ src/aind_analysis_arch_result_access/patch/patch_20250922_redo_Bowen_sessions.py
@@ -4,6 +4,7 @@ import logging
 
 from codeocean import CodeOcean
 from codeocean.data_asset import DataAssetAttachParams
+import shutil
 
 logger = logging.getLogger(__name__)
 
@@ -32,9 +33,34 @@ def attach_assets(asset_ids):
     logger.info(f"Attached {len(mounted)} assets successfully")
     return mounted
 
+def extract_all_nwbs(mounted):
+    # Copy the nwb file from mounted_directory/nwb to /results/extracted_Bowen_nwbs
+    output_dir = '/results/extracted_Bowen_nwbs'
+    os.makedirs(output_dir, exist_ok=True)
+    for asset in mounted:
+        try:
+            nwb_source = os.path.join('/root/capsule/data', asset['mount'], 'nwb')
+            if os.path.exists(nwb_source):
+                for nwb_dir in os.listdir(nwb_source):
+                    dir_path = os.path.join(nwb_source, nwb_dir)
+                    if os.path.isdir(dir_path):
+                        dst_dir = os.path.join(output_dir, nwb_dir)
+                        logger.info(f"Copying directory {dir_path} to {dst_dir}")
+                        if os.path.exists(dst_dir):
+                            shutil.rmtree(dst_dir)
+                        shutil.copytree(dir_path, dst_dir)
+            else:
+                logger.warning(f"No 'nwb' directory found in asset {asset['id']} at {nwb_source}")
+        except Exception as e:
+            logger.error(f"Failed to extract NWB files from asset {asset['id']}: {e}")
+            
+
 if __name__ == "__main__":
     
     logging.basicConfig(level=logging.INFO)
 
     asset_ids = get_asset_ids()
     mounted = attach_assets(asset_ids)
+    
+    extract_all_nwbs(mounted)
+    
